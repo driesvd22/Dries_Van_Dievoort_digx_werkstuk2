7c498f81d6ab6693a2aba01f9c802590b07fe23a
driesvd22 <driesvandievoort8@gmail.com>
2018-05-21 23:26:34 +0200

    .

--- Dries_Van_Dievoort_digx_werkstuk2/Dries_Van_Dievoort_digx_werkstuk2/ViewController.swift
+++ Dries_Van_Dievoort_digx_werkstuk2/Dries_Van_Dievoort_digx_werkstuk2/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = URL(string: "https://api.jcdecaux.com/vls/v1/stations?contract=Bruxelles-Capitale&apiKey=0f6eeb84f56a0ec96b79278e957afed918b2d6db")
+        let task = URLSession.shared.dataTask(with: url!){(data, response, error) in
+            if error != nil
+            {
+                print("ERROR")
+            }
+            else
+            {
+                if let content = data
+                {
+                    do
+                    {
+                        let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        let location = myJSON["position"] as? AnyObject
+                        print(location)
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+        }
+            task.resume()
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
